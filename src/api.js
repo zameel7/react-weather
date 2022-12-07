f66af016d8f0f4232eee57b2a3cbf0c22c71d020
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2022-12-07 23:44:50 +0530

    initial

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4bb347b9a0msh81be885d2a4d6f0p1bc4e1jsnba3533c544fb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,15 @@
+import "./current-weather.css"
+
+const CurrentWeather = () => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">Belgrade</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+            <img alt="weather" className="weather-icon"></img>
+        </div>
+    );
+}
+
+export default CurrentWeather;
